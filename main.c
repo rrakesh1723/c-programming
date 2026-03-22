c218c7ca16b255aa154344661e02baa90b660b2d
rrakesh1723 <rrakesh1723@gmail.com>
2026-03-22 16:47:11 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    FILE*fp;
-    fp=fopen("charfile.txt","w");
-    if(fp==NULL){
-        printf("Unable to open file!\n");
-     return 1;
-    }
-    fputc('A',fp);
-    fputc('\n',fp);
-    fputc('B',fp);
-    fclose(fp);
-    printf("Characters written successfully.\n");
-    return 0;
-}
+void sayHello(){
+ printf("Hello,student!You just called me back!\n");
+ }
+
+ void studentwork("void(*callbackfunc)()){
+      printf("Student is doing homework...\n");
+        callbackfunc();
+
+      int main(){
+      studentwork(sayHello);
+      return 0;
+      }
+
---
+++ print elements program.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void display(int arr[],int n){
+int i;
+printf("Array elements:");
+for(i=0;i<n;i++)
+    printf("\n%d",arr[i]);
+}
+int main(){
+int arr[5]={10,20,30,40,50};
+display(arr,5);
+return 0;
+}
+
+
